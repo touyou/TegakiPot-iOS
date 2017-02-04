960de17adc155bfc4c76517c2c80ac89d5871082
Fujii Yosuke <fujiyou1121@gmail.com>
2017-02-04 18:08:10 +0900

    recover files

---
+++ TegakiPot-iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TegakiPot-iOS
+//
+//  Created by 藤井陽介 on 2017/02/04.
+//  Copyright © 2017年 touyou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
