ff212242a5295f8ee5d078d4c23fe7486c1a152b
Fujii Yosuke <fujiyou1121@gmail.com>
2017-02-04 13:55:30 +0900

    modify some change and add TegakiPotAPI

---
+++ TegakiPot-iOS/Model/API/TegakiPotAPI.swift
@@ -0,0 +1,15 @@
+//
+//  TegakiPotAPI.swift
+//  TegakiPot-iOS
+//
+//  Created by 藤井陽介 on 2017/02/04.
+//  Copyright © 2017年 touyou. All rights reserved.
+//
+
+import Foundation
+import APIKit
+import Himotoki
+
+class TegakiPotAPI {
+    
+}
