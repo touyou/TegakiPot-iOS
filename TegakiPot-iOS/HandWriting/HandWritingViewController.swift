e97b317a0bba940c2f87d6b8716999c18ee8f267
Fujii Yosuke <fujiyou1121@gmail.com>
2017-02-05 10:11:27 +0900

    add color button

--- TegakiPot-iOS/HandWriting/HandWritingViewController.swift
+++ TegakiPot-iOS/HandWriting/HandWritingViewController.swift
@@ -14,6 +14,13 @@ class HandWritingViewController: UIViewController {
     var svg: AEXMLDocument?
     var delegate: EditQuestionDelegate!
     
+    @IBOutlet var colorButton: [UIButton]!
+    var colors = [MaterialColor.red.color, MaterialColor.purple.color, MaterialColor.blue.color, MaterialColor.green.color, MaterialColor.orange.color, MaterialColor.black.color]
+    var selected = 0
+    var sizeFlag = 0
+    var sizeArray = [40.0, 80.0, 120.0]
+    var centerPoint: CGPoint!
+    
     @IBOutlet weak var drawableView: UIView! {
         didSet {
             drawableView.layer.masksToBounds = false
@@ -51,11 +58,39 @@ class HandWritingViewController: UIViewController {
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
+        
+        for i in 0 ..< colorButton.count {
+            colorButton[i].tag = i
+            colorButton[i].frame.size = CGSize(width: 40.0, height: 40.0)
+            colorButton[i].backgroundColor = colors[i]
+        }
+        
+        centerPoint = CGPoint(x: colorButton[0].frame.origin.x + 20.0, y: colorButton[0].frame.origin.y + 20.0)
+        colorButton[0].layer.cornerRadius = 20.0
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func tapColorButton(_ sender: UIButton) {
+        if sender.tag == selected {
+            let aft = CGFloat(sizeArray[(sizeFlag + 1) % 3])
+            sender.frame = CGRect(x: centerPoint.x - aft / 2, y: centerPoint.y - aft / 2,
+                                  width: aft, height: aft)
+            sender.layer.cornerRadius = aft / 2
+            sizeFlag = (sizeFlag + 1) % 3
+        } else {
+            colorButton[selected].frame = CGRect(x: centerPoint.x - 20.0, y: centerPoint.y - 20.0,
+                                              width: 40.0, height: 40.0)
+            colorButton[selected].layer.cornerRadius = 0.0
+            centerPoint = CGPoint(x: sender.frame.origin.x + 20.0,
+                                  y: sender.frame.origin.y + 20.0)
+            sender.layer.cornerRadius = 20.0
+            selected = sender.tag
+            sizeFlag = 0
+        }
     }
     
     @IBAction func undo() {
